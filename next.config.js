4c714bb11dbda0a97cf61f38fb858d35d3112196
stephaneazoulay <stephaneazoulay@hotmail.com>
2025-04-18 15:12:17 +0200

    Fix: disable lightningcss for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     output: 'standalone',
+    experimental: {
+      optimizeCss: false, // Désactive l'optimisation CSS, ce qui peut résoudre le problème avec LightningCSS
+    },
   };
   
